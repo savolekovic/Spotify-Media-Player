499f9a4de6c96a165413bacf03dc096673a5d89d
Savo Lekovic <savo.lekovic@amplitudo.me>
2025-08-08 15:25:58 +0200

    add docker

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const path = require('path');
 
 const app = express();
@@ -11,11 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const options = {
-    key: fs.readFileSync('../localhost-key.pem'),
-    cert: fs.readFileSync('../localhost.pem')
-};
-
-https.createServer(options, app).listen(3000, () => {
-    console.log('HTTPS Server running on https://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`HTTP Server running on port ${port}`);
 });
